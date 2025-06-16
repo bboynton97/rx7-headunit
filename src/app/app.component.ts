b023b92e1d3843d0df6a7895b51bad13c2650d25
Braelyn Boynton <bboynton97@gmail.com>
2025-06-16 11:56:07 +0100

    no cursor... mostly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,15 @@ import { invoke } from "@tauri-apps/api/core";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  greetingMessage = "";
 
-  greet(name: string): void {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
+  log = "hello";
+  constructor() {
+    getCurrentWindow().setCursorVisible(false);
   }
 
-    playSound() {
-      invoke<void>("play_synth").then(r => {
-        console.log("sound played");
-      });
-    }
+  playSound() {
+    invoke<void>("play_synth").then(r => {
+      console.log("sound played");
+    });
+  }
 }
